222600ec1121d0455a0257626eb0e21c7f1d2ff5
billelkhanfr <khanfri.billel@yahoo.fr>
2024-02-16 22:13:12 +0100

    update slider images

--- src/home/pages/HomePage.jsx
+++ src/home/pages/HomePage.jsx
@@ -21,15 +21,17 @@ function HomePage() {
   }, []);
 
   // const slide1 = "https:rm83.fr/balade/wp-content/uploads/2024/01/les-halles-vue-arriere.jpg"
+
+
+
   const slide1 =
-      "https://firebasestorage.googleapis.com/v0/b/khanfri-billel.appspot.com/o/D_Home.png?alt=media&token=ead9e575-824a-4d5c-98eb-164b6afc2422"
+    "https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
 ;
   const slide2 =
-  "https://firebasestorage.googleapis.com/v0/b/khanfri-billel.appspot.com/o/Capture%20d%E2%80%99e%CC%81cran%202024-02-15%20a%CC%80%2014.06.18.PNG?alt=media&token=8c9f50f0-fdbb-4ee8-8643-e9b489b2df65";
-  const slide3 =
+"https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80";  const slide3 =
      "https://firebasestorage.googleapis.com/v0/b/khanfri-billel.appspot.com/o/Capture%20d%E2%80%99e%CC%81cran%202024-02-15%20a%CC%80%2018.45.56.png?alt=media&token=87abc8ab-870b-45c8-b8bf-fe67d6b69a36";
 const slide4 =
-  "https://firebasestorage.googleapis.com/v0/b/khanfri-billel.appspot.com/o/Capture%20d%E2%80%99e%CC%81cran%202024-02-15%20a%CC%80%2018.51.52.png?alt=media&token=8db37e6b-2c8d-44ef-ba12-2e9e9d4ed0b7";
+    "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80";
 
   const slides = [slide1, slide2, slide3,slide4];
   return (
