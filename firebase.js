a9e79a37b3d6b36ce6cf01ee55592487e4c0dccd
billelkhanfr <khanfri.billel@yahoo.fr>
2024-02-27 21:54:02 +0100

    env keys

--- firebase.js
+++ firebase.js
@@ -8,18 +8,19 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDfGBuJBcOD_tqEhgEihSVgslrXoB7I9A0",
-  authDomain: "billel-khanfri.firebaseapp.com",
-  projectId: "billel-khanfri",
-  storageBucket: "billel-khanfri.appspot.com",
-  messagingSenderId: "904885973248",
-  appId: "1:904885973248:web:ed56f20d904822099783e5",
-  measurementId: "G-461VNFNCJW",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
-export { app, db,analytics };
+export { app, db, analytics };
